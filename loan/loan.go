cda1086cfef4a750e0e9cf8e891373bf2fadb289
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-12-26 11:04:41 +0100

    Add first passing test (faking it for now)

--- loan/loan.go
+++ loan/loan.go
@@ -3,15 +3,32 @@ package loan
 
 import "github.com/shopspring/decimal"
 
+// Error represents an enumeration of errors returned by the loan
+// package. These errors can be used as error sentinels to check
+// for specific classes of errors.
+//
+// Always use errors.Is to check since the error sentinel will be
+// wrapper with more context.
+type Error string
+
+const (
+	// ErrInvalidParameter is returned when on of the parameters passed is invalid.
+	ErrInvalidParameter Error = "invalid parameter"
+)
+
 // CalculateAnnuity will calculate the annuity payment according to the
 // formula described here: https://financeformulas.net/Annuity_Payment_Formula.html
 //
 // It returns an error if any of the parameters is invalid, like the duration
 // in months being zero.
 func CalculateAnnuity(
-	durationInMonths uint,
-	annualInterestRate decimal.Decimal,
 	totalLoanAmount decimal.Decimal,
+	annualInterestRate decimal.Decimal,
+	durationInMonths uint,
 ) (decimal.Decimal, error) {
-	return decimal.Decimal{}, nil
+	return decimal.Decimal{}, ErrInvalidParameter
+}
+
+func (e Error) Error() string {
+	return string(e)
 }
---
+++ loan/loan_test.go
@@ -0,0 +1,69 @@
+package loan_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/katcipis/loaner/loan"
+	"github.com/shopspring/decimal"
+)
+
+// Here you can find some table driven tests, I'm very fond to them :-).
+// This presentation talks a little about them
+// (and some other interesting ideas): https://www.youtube.com/watch?v=8hQG7QlcLBk
+
+func TestAnnuityCalculation(t *testing.T) {
+
+	// Test represents a single test case for the annuity calculation
+	// We use strings here to make the test descriptor more concise and
+	// easier to read as a high level behavior descriptor.
+	type Test struct {
+		name               string
+		totalLoanAmount    string
+		annualInterestRate string
+		durationInMonths   uint
+		want               string
+		wantErr            error
+	}
+
+	tests := []Test{
+		{
+			name:               "ErrorIfDurationIsZero",
+			totalLoanAmount:    "500.00",
+			annualInterestRate: "3.0",
+			durationInMonths:   0,
+			wantErr:            loan.ErrInvalidParameter,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			loanAmount := toDecimal(t, test.totalLoanAmount)
+			interestRate := toDecimal(t, test.annualInterestRate)
+			want := decimal.Decimal{}
+
+			if test.want != "" {
+				want = toDecimal(t, test.want)
+			}
+
+			got, err := loan.CalculateAnnuity(loanAmount, interestRate, test.durationInMonths)
+
+			if !errors.Is(err, test.wantErr) {
+				t.Errorf("got error %v; want %v", err, test.wantErr)
+			}
+
+			if !got.Equal(want) {
+				t.Errorf("got result %v; want %v", got, test.want)
+			}
+		})
+	}
+}
+
+func toDecimal(t *testing.T, v string) decimal.Decimal {
+	t.Helper()
+	d, err := decimal.NewFromString(v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return d
+}
